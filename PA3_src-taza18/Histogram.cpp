592148d81b63ac169e5b7796f1949de3d521446c
Murtaza Hakimi <murtaza.hakimi@gmail.com>
2018-10-17 12:52:25 -0500

    make functions thread safe

--- PA3_src-taza18/Histogram.cpp
+++ PA3_src-taza18/Histogram.cpp
@@ -22,12 +22,16 @@ void Histogram::update (string request, string response){
 	Is this function thread-safe???
 	Make necessary modifications to make it thread-safe
 	*/
+	std::lock_guard<std::mutex> l(_mtx);
+
 	int person_index = map [request];
 	hist [person_index][stoi(response) / 10] ++;
 }
 void Histogram::print(){
 	cout << setw(10) << right << "Range";
 	for (int j=0; j<3; j++){
+		std::lock_guard<std::mutex> l(_mtx);
+		
 		cout << setw(15) << right << names [j];
 	}
 	cout <<endl;
@@ -38,6 +42,8 @@ void Histogram::print(){
 		string range = "[" + to_string(i*10) + " - " + to_string((i+1)*10 - 1) + "]:"; 
 		cout << setw (10) << right << range;
 		for (int j=0; j<3; j++){
+			std::lock_guard<std::mutex> l(_mtx);
+
 			cout << setw(15) << right << hist [j] [i];
 			sum [j] += hist [j] [i];
 		}
