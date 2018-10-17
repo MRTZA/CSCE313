3e2dcc6104d8535f9ff7ff00d94a65bb5d4b0b6b
Murtaza Hakimi <murtaza.hakimi@gmail.com>
2018-10-17 11:46:28 -0500

    make class thread safe

--- PA3_src-taza18/SafeBuffer.h
+++ PA3_src-taza18/SafeBuffer.h
@@ -4,11 +4,22 @@
 #include <stdio.h>
 #include <queue>
 #include <string>
+#include <mutex>
+
 using namespace std;
 
 class SafeBuffer {
 private:
-	queue<string> q;	
+	queue<string> q;
+
+    /************************************************************
+     * 
+     * Make a class thread safe:
+     * https://mfreiholz.de/posts/make-a-class-thread-safe-cpp/
+     * 
+     * *********************************************************/
+    mutable mutex _mtx;
+
 public:
     SafeBuffer();
 	~SafeBuffer();
