e0edeec46b0b672a38963b62552c33aa851b4fbe
Murtaza Hakimi <murtaza.hakimi@gmail.com>
2018-10-30 18:46:45 -0500

    thread safe and conditions

--- PA4_src-taza18_v2/BoundedBuffer.cpp
+++ PA4_src-taza18_v2/BoundedBuffer.cpp
@@ -4,7 +4,7 @@
 using namespace std;
 
 BoundedBuffer::BoundedBuffer(int _cap) {
-	
+	max_size = _cap;
 }
 
 BoundedBuffer::~BoundedBuffer() {
@@ -20,14 +20,46 @@ void BoundedBuffer::push(string str) {
 	Is this function thread-safe??? Does this automatically wait for the pop() to make room 
 	when the buffer if full to capacity???
 	*/
+	
+	// lock mutex
+	pthread_mutex_lock (&m);
+
+	// check for overflow
+	while (size () == max_size){
+        pthread_cond_wait(&prod, &m);
+    }
+
+	// now produce
 	q.push (str);
+
+	// send signal to Consumer(s)
+	pthread_cond_signal (&cons);
+
+	// unlock mutex
+	pthread_mutex_unlock (&m);
 }
 
 string BoundedBuffer::pop() {
 	/*
 	Is this function thread-safe??? Does this automatically wait for the push() to make data available???
 	*/
+	
+	// lock mutex
+	pthread_mutex_lock (&m);
+
+	// check for underflow
+	while (size () == 0){
+        pthread_cond_wait(&cons, &m);
+    }
+
+    // now consume
 	string s = q.front();
 	q.pop();
+
+	// send signal to Producer(s)
+	pthread_cond_signal (&prod);
+
+	// unlock mutex
+	pthread_mutex_unlock (&m);
 	return s;
 }
